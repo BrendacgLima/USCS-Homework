1b801a617e8a2a162b5ab1d1f32fd63010e33112
BrendacgLima <99383086+BrendacgLima@users.noreply.github.com>
2022-11-17 20:09:20 -0300

    Add files via upload

---
+++ Tabela ASCII.c
@@ -0,0 +1,19 @@
+# include  < stdio.h >
+
+ intervalo int ( caractere n){
+    int retorno= 0 ;
+    if (n>= ' a ' && n<= ' z ' ){
+        retorno= 1 ;
+    }
+    retorno retorno;
+}
+
+int  principal ()
+{
+    caracter n; resultado int ;
+    scanf ( " %c " ,&n);
+    resultado= intervalo (n);
+    printf ( " %d " ,resultado);
+
+    retorna  0 ;
+}
